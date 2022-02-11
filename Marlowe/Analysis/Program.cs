c2b9452a7ff65bc2b9737b99bac482e7c4e9f508
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-02-11 01:20:06 +0000

    rewrote entrypoint to work with updated Analyser

--- Marlowe/Analysis/Program.cs
+++ Marlowe/Analysis/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace Marlowe.Analysis
 {
@@ -10,12 +10,23 @@ namespace Marlowe.Analysis
     {
         static void Main(string[] args)
         {
-            try
-            {
-
+            string fileName = "dummy.ss";
+            string FileContents = File.ReadAllText(fileName);
+            Analyser analyser = new Analyser(FileContents);
+            try{
+                analyser.CommonTokenStream.Fill();
+                IList<IToken> tokens = analyser.CommonTokenStream.GetTokens();
+                foreach (IToken token in tokens){
+                    ITokenSource source = token.TokenSource;
+                    Console.WriteLine("Channel: "+ token.Channel);
+                    Console.WriteLine("Text:\t" + token.Text);
+                    Console.WriteLine("Line:\t" + token.Line);
+                    Console.WriteLine("Column:\t" + token.Column);
+                }
+                CSharpParser.Compilation_unitContext context = analyser.Parser.compilation_unit();
+                ITerminalNode[] contextTokens = context.GetTokens(87);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex){
                 Console.WriteLine("Error: " + ex);
             }
         }
