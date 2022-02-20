cf0809ca7d4a4b09d096b73da608b480e8e37740
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-02-20 01:44:10 +0000

    added visitor for debugging

--- Marlowe/Analysis/Program.cs
+++ Marlowe/Analysis/Program.cs
@@ -8,13 +8,18 @@ namespace Marlowe.Analysis
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args){
             string fileName = "dummy.ss";
-            string FileContents = File.ReadAllText(fileName);
+            //string FileContents = File.ReadAllText(fileName);
+            string FileContents = "public class dummy{\n\rint x = 2 + 4;";
             Analyser analyser = new Analyser(FileContents);
             try{
                 analyser.CommonTokenStream.Fill();
+                CSharpParser codeParser= analyser.Parser;
+                global::CSharpParser.Compilation_unitContext context = codeParser.compilation_unit();
+                analyser.Visitor.VisitCompilation_unit(context);
+                codeParser.AddErrorListener(new BaseErrorListener());
+                Dictionary<string, Node> keys = analyser.Visitor.Variables;
                 IList<IToken> tokens = analyser.CommonTokenStream.GetTokens();
                 foreach (IToken token in tokens){
                     ITokenSource source = token.TokenSource;
@@ -23,8 +28,9 @@ namespace Marlowe.Analysis
                     Console.WriteLine("Line:\t" + token.Line);
                     Console.WriteLine("Column:\t" + token.Column);
                 }
-                CSharpParser.Compilation_unitContext context = analyser.Parser.compilation_unit();
-                ITerminalNode[] contextTokens = context.GetTokens(87);
+
+              //  CSharpParser.Compilation_unitContext context = analyser.Parser.compilation_unit();
+              //  ITerminalNode[] contextTokens = context.GetTokens(87);
             }
             catch (Exception ex){
                 Console.WriteLine("Error: " + ex);
