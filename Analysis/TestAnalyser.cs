5de829f00b87e46977a97a10a86b6b4590182b9b
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-03-27 17:54:31 +0100

    construction of testing suite

---
+++ Analysis/TestAnalyser.cs
@@ -0,0 +1,33 @@
+﻿using Antlr4.Runtime;
+using Marlowe.Visitor;
+
+namespace Marlowe.Tests
+{
+    internal abstract class TestAnalyser
+    {
+        #region Attribs
+        private AntlrInputStream stream;
+        private CSharpLexer lexer;
+        private CSharpParser parser;
+        private CSharpVisitor visitor;
+        private CommonTokenStream commonTokenStream;
+        #endregion
+
+        public void Setup(string text)
+        {
+            stream = new AntlrInputStream(text);
+            lexer = new CSharpLexer(stream);
+            commonTokenStream = new CommonTokenStream(lexer);
+            parser = new CSharpParser(commonTokenStream);
+            visitor = new CSharpVisitor();
+        }
+
+
+        #region Getter & Setters
+        public CSharpLexer Lexer { get => lexer; }
+        public CSharpParser Parser { get => parser; }
+        public CSharpVisitor Visitor { get => visitor; }
+        public CommonTokenStream CommonTokenStream { get => commonTokenStream; }
+        #endregion
+    }
+}
---
+++ Analysis/Vistior Testing/NamespaceTests.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Marlowe.Tests
+{
+    [TestClass]
+    internal class NamespaceTests : TestAnalyser{ 
+
+
+    }
+}
