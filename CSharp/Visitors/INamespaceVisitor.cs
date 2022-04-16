9d92ccbbbfbd303066ee2bd352a44a16b67015f8
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-04-16 01:48:06 +0100

    Potential decoupling of CSharpVisitor

---
+++ CSharp/Visitors/INamespaceVisitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Antlr4.Runtime.Misc;
+using CSharp;
+
+namespace Marlowe.Visitor
+{
+    public interface INamespaceVisitor<T>
+    {
+        //   namespace_member_declarations
+        //:       namespace_member_declaration                      
+        public T VisitNamespace_member_declarations([NotNull] CSharpParser.Namespace_member_declarationsContext context);
+
+        //namespace_member_declaration
+        //: namespace_declaration
+        //| type_declaration
+        //;
+        public T VisitNamespace_member_declaration([NotNull] CSharpParser.Namespace_member_declarationContext context);
+
+        //namespace_declaration
+        //: NAMESPACE qi = qualified_identifier namespace_body ';'?
+        public T VisitNamespace_declaration([NotNull] CSharpParser.Namespace_declarationContext context);
+        // namespace_body
+        // OPEN_BRACE extern_alias_directives? using_directives? namespace_member_declarations? CLOSE_BRACE
+        public T VisitNamespace_body([NotNull] CSharpParser.Namespace_bodyContext context);
+    }
+}
--- CSharp/Visitors/NamespaceVisitor.cs
+++ CSharp/Visitors/NamespaceVisitor.cs
@@ -1,7 +1,8 @@
 ﻿using Antlr4.Runtime.Misc;
+using Marlowe.Utilities;
 using System.Collections.Generic;
 
-namespace CSharp
+namespace Marlowe.Visitor
 {
     /** Details
       *  The Compilation Process is divided into the following:
@@ -10,46 +11,46 @@ namespace CSharp
       *  
       *  This class represents the namespace_member_declarations Context and its Tree
       */
-    public abstract class VisitorNamespace_Member_Declarations : CSharpParserBaseVisitor<object?>
+    public abstract class NamespaceVisitor : INamespaceVisitor<object>
     {
+        #region Namespace
         //   namespace_member_declarations
         //:       namespace_member_declaration                      
-        public override object? VisitNamespace_member_declarations([NotNull] CSharpParser.Namespace_member_declarationsContext context){
+        public object VisitNamespace_member_declarations([NotNull] CSharpParser.Namespace_member_declarationsContext context){
 
             foreach (var NamespaceContext in context.namespace_member_declaration()){
-                if (NamespaceContext != null)
-                {
+                if (NamespaceContext != null){
                     VisitNamespace_member_declaration(NamespaceContext);
                 }
             }
 
-            return null;
+            return default;
         }
 
         //namespace_member_declaration
         //: namespace_declaration
         //| type_declaration
         //;
-        public override object? VisitNamespace_member_declaration([NotNull] CSharpParser.Namespace_member_declarationContext context){
+        public object VisitNamespace_member_declaration([NotNull] CSharpParser.Namespace_member_declarationContext context){
 
             if (context.namespace_declaration() != null){
                 VisitNamespace_declaration(context.namespace_declaration());
             }
             else if (context.type_declaration() != null){
-                VisitType_declaration(context.type_declaration());
+                //VisitType_declaration(context.type_declaration());
             }
             return null;
         }
 
 
-        public override object? VisitNamespace_declaration([NotNull] CSharpParser.Namespace_declarationContext context)
+        public object VisitNamespace_declaration([NotNull] CSharpParser.Namespace_declarationContext context)
         {
             if (!context.IsEmpty)
             {
                 if (context.NAMESPACE() != null)
                 {
                     if (context.namespace_body() != null){
-                        return new Dictionary<string, object?> { { context.SEMICOLON().GetText(), context.namespace_body() } };
+                        //return new Dictionary<string, object> { { context.SEMICOLON().GetText(), context.namespace_body() } };
 
                     }
                 }
@@ -57,16 +58,16 @@ namespace CSharp
             return null;
         }
 
-        public override object? VisitNamespace_body([NotNull] CSharpParser.Namespace_bodyContext context)
+        public object VisitNamespace_body([NotNull] CSharpParser.Namespace_bodyContext context)
         {
             if (context.OPEN_BRACE() != null)
             {
                 if(context.CLOSE_BRACE() != null){
                     if (context.extern_alias_directives() != null){
-                        VisitExtern_alias_directives(context.extern_alias_directives());
+                      //  VisitExtern_alias_directives(context.extern_alias_directives());
                     }
                     if (context.using_directives() != null) { 
-                        VisitUsing_directives(context.using_directives());
+                       // VisitUsing_directives(context.using_directives());
                     }
                     if (context.namespace_member_declarations() != null){
                         VisitNamespace_member_declarations(context.namespace_member_declarations());
@@ -84,7 +85,9 @@ namespace CSharp
         }
 
 
+    
 
+        #endregion
 
     }
 }
