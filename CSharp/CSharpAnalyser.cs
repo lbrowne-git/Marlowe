84efa1f2ac5aa930e9f481dea1b01a217e6d8cd6
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-04-18 19:03:20 +0100

    Reworking of Analyser as should be abstract instead of interface

--- CSharp/CSharpAnalyser.cs
+++ CSharp/CSharpAnalyser.cs
@@ -7,38 +7,25 @@ namespace Marlowe.CSharp
 {
     /**
      * Summary:
-     *      The CSharp implementation of the IAnalyser interface, which acts as a
+     *      The CSharp implementation of the abstract Analyser, which acts as a
      *      contoller for the CSharp Lexer and Parser with the input source code.
      * 
      */
-    public class CSharpAnalyser : IAnalyser
+    public class CSharpAnalyser : Analyser
     {
-        #region Attribs
-        private AntlrInputStream stream;
-        private Lexer lexer;
-        private Parser parser;
-        private CommonTokenStream commonTokenStream;
         private CSharpSemanticAnalyser visitor;
-        #endregion
-        
+
         private void Setup(string text){
-            stream = new AntlrInputStream(text);
-            lexer = new CSharpLexer(stream);
-            commonTokenStream = new CommonTokenStream(lexer);
-            parser = new CSharpParser(commonTokenStream);
-            visitor = new CSharpSemanticAnalyser();
+            
+            Stream = new AntlrInputStream(text);
+            Lexer = new CSharpLexer(Stream);
+            CommonTokenStream = new CommonTokenStream(Lexer);
+            Parser = new CSharpParser(CommonTokenStream);
+            Visitor = new CSharpSemanticAnalyser();
         }
         public CSharpAnalyser(string text){
             Setup(text);
         }
-
-        #region Getter & Setters
-        public Lexer Lexer { get => lexer; }
-        public Parser Parser { get => parser; }
-        public IParseTreeVisitor<object?> Visitor { get => visitor; }
-        public CommonTokenStream CommonTokenStream { get => commonTokenStream; }
-        #endregion
-
     }
 }
  
---
+++ Utilities/Analyser.cs
@@ -0,0 +1,22 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace Marlowe.Utilities
+{
+
+    /**
+     *      An intermediary layer for all Lexers and Parsers. it provides connections
+     *          to abstract implementation of ANTLR4's run time libraries, which
+     *          can be used to extend this analyser for any language built this way.
+     */
+    public abstract class Analyser
+    {
+        public virtual AntlrInputStream Stream { set; get; }
+        public virtual Lexer Lexer { set; get ; }
+        public virtual Parser Parser { set; get; }
+        public virtual IParseTreeVisitor<object?> Visitor { set;get ; }
+        public virtual CommonTokenStream CommonTokenStream { set; get ; }
+
+        public int MyProperty { get; set; }
+    }
+}
