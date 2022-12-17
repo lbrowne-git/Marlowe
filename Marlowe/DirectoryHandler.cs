3d002435f70be6b7ccbfe2f852f602dd2889829c
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-12-17 01:54:25 +0000

    added subdirectory functionality and decoupled from cli functionality

---
+++ Marlowe/DirectoryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace Marlowe
+{
+    public static class DirectoryHandler
+    {
+        private static List<string> fileInformation = new List<string>();
+        public static List<string> GatherFileInformation(string path, bool checkSubDirectories)
+        {
+            if(checkSubDirectories)
+            {
+                GatherInformationAboutSubDirectories(path);
+            }
+
+            return fileInformation;
+        }
+
+        private static void GatherInformationAboutSubDirectories(string path)
+        {
+            if (Directory.GetDirectories(path).Length > 0)
+            {
+                foreach (string directory in Directory.GetDirectories(path))
+                {
+                    GatherInformationAboutSubDirectories(directory);
+                }
+            }
+            GatherInformationAboutFilesInFolder(path);
+            
+        }
+
+        private static void GatherInformationAboutFilesInFolder(string path)
+        {
+
+            foreach (string file in Directory.GetFiles(path, "*.cs"))
+            {
+                if (System.IO.File.Exists(file))
+                {
+                    fileInformation.Add(file);
+                }
+            }
+        }
+    }
+}
