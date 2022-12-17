8ee9c23af7350c39314dcedee00d5f3c1392a418
lbrowne-git <lewiskerinbrowne1@gmail.com>
2022-12-17 02:22:43 +0000

    added documentation and clean up

--- Marlowe/DirectoryHandler.cs
+++ Marlowe/DirectoryHandler.cs
@@ -1,26 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Net.WebRequestMethods;
 
 namespace Marlowe
 {
+
+    /// <summary>
+    ///    A directory handler that discovers C# files in a given directory. It's only current use is the <see cref="GatherFileInformation(string, bool)"/> function.
+    /// </summary>
     public static class DirectoryHandler
     {
-        private static List<string> fileInformation = new List<string>();
-        public static List<string> GatherFileInformation(string path, bool checkSubDirectories)
+        private readonly static List<string> FileInformation = new List<string>();
+
+        /// <summary>
+        ///     Searches a given directory for C# files and returns a list of absolute directories to these files.
+        ///     It is predominately used in this application by the <see cref="Interpeter"/>.
+        /// </summary>
+        /// <param name="path">Directory to files to be read.</param>
+        /// <param name="checkSubDirectories">Toggleable sub directory search</param>
+        /// <returns>A list of directories for C# files.</returns>
+        public static List<string> GatherFileInformation(string path, bool checkSubDirectories = true)
         {
             if(checkSubDirectories)
             {
                 GatherInformationAboutSubDirectories(path);
             }
 
-            return fileInformation;
+            return FileInformation;
         }
 
+        //recursively searches file directories for subdirectories and then adds files to list.
         private static void GatherInformationAboutSubDirectories(string path)
         {
             if (Directory.GetDirectories(path).Length > 0)
@@ -39,9 +47,9 @@ namespace Marlowe
 
             foreach (string file in Directory.GetFiles(path, "*.cs"))
             {
-                if (System.IO.File.Exists(file))
+                if (File.Exists(file))
                 {
-                    fileInformation.Add(file);
+                    FileInformation.Add(file);
                 }
             }
         }
